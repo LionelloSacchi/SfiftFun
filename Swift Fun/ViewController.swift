e4e7cc3e024f0b002a25164a0ed9ec116e8f95bd
Lionello Sacchi <l.sacchi@aros-solar.com>
2018-06-15 13:47:50 +0200

    Primo commit su git.

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonCount = 0
+    
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    @IBAction func ButtonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print (buttonCount)
+        
+        if  buttonCount >= 10 {
+            view.backgroundColor = UIColor.red
+            coolLabel.text = "COOL!"
+        }
+        
+        if  buttonCount >= 15 {
+            view.backgroundColor = UIColor.green
+            coolLabel.text = "COOLER!"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
